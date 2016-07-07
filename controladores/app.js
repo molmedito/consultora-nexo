839f7b847c9f79df16bfe3d9aaa770666243c989
Marcelo Olmedo <marcelo.a.olmedo@gmail.com>
2016-07-06 23:00:22 -0300

    Detalles de cliente 1/3

--- controladores/app.js
+++ controladores/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('prosales', ['ngRoute', 'ngAnimate']);
+var app = angular.module('consultoraNexo', ['ngRoute', 'ngAnimate']);
 
 app.config(['$routeProvider', function($routeProvider){
     $routeProvider
